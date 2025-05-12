81d2db68f83be87f874484a93a87c01b050989b6
dedyhariadi <dedyhariadi@gmail.com>
2025-05-12 16:36:22 +0700

    menambahkan master template

---
+++ app/Database/Migrations/2025-05-12-090257_Users.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '250',
+            ],
+            'username' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '250',
+            ],
+            'password' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '250',
+            ],
+            'level' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '20',
+            ]
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        //
+    }
+}
