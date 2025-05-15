05b241f781fcd54fcf0be03854e16357e491457c
dedyhariadi <dedyhariadi@gmail.com>
2025-05-15 09:12:13 +0700

    membuat halaman home

---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,20 @@
+ <!-- navbar -->
+ <nav id="navbar-example2" class="navbar px-3 bg-primary" data-bs-theme="dark">
+     <div class="container">
+         <a class="navbar-brand" href="#">
+             <h2>e-HPS</h2>
+         </a>
+         <ul class="nav nav-pills">
+             <li class="nav-item">
+                 <a class="nav-link active" href="">Home</a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="">Barang</a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="">Kegiatan</a>
+             </li>
+         </ul>
+     </div>
+ </nav>
+ <!-- akhir navbar -->
