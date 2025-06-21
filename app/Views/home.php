9865878451a9c8e50f7fa4e5a3c8b5d97760ad49
dedyhariadi <dedyhariadi@gmail.com>
2025-06-21 07:07:28 +0700

    update logo

--- app/Views/home.php
+++ app/Views/home.php
@@ -6,9 +6,9 @@
 
 <!-- awal jumbotron -->
 <section class="jumbotron text-center">
-    <img src="/assets/images/logo2.png" alt="e-Harga Perkiraan Sendiri" width="250" class="rounded-circle mt-1">
-    <h1 class="display-1 mt-4">Arsenal</h1>
-    <p class="lead fs-4"> Simple &nbsp;&nbsp;&&nbsp;&nbsp; Effortless</p>
+    <img src="/assets/images/e-siap.png" alt="e-Harga Perkiraan Sendiri" width="250" class="rounded-circle mt-1">
+    <h1 class="display-1 mt-4 fw-bolder">e-Siap</h1>
+    <p class="lead fs-3"> Arsenal - Dissenlekal</p>
 
     <!-- bentuk gelombang  | ambil dari getwaves.io-->
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
